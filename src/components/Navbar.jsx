b034be51dc1b06ad600d19fe47c624a16cc98918
Pratt <pratt.paredes@gmail.com>
2022-10-29 13:30:32 -0500

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,53 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Navbar.css";
+import Logo from '../assets/Logo.png'
 
 function Navbar() {
+  let navigate = useNavigate();
+
   return (
     <div>
-        
+      <nav>
+        <div className="nav__container">
+          <figure className="nav__logo--wrapper">
+            <img src={Logo} alt="" className="nav__logo" />
+          </figure>
+          <ul className="nav__links">
+            <button
+              className="
+                    nav__link 
+                    link__hover-effect
+                    link__hover-effect--blue
+                    "
+              onClick={() => navigate("/")}
+            >
+              Home
+            </button>
+            <button
+              className="
+                    nav__link 
+                    link__hover-effect
+                    link__hover-effect--black
+                    "
+              onClick={() => navigate("/search")}
+            >
+              Find your Recipe
+            </button>
+            <button
+              className="
+                    nav__link 
+                    nav__link--btn
+                    "
+              onClick={() => alert("Function not implemented")}
+            >
+              Contact
+            </button>
+          </ul>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Header from '../components/Header'
 import Navbar from '../components/Navbar'
 
 function Home() {
   return (
-    <div>
+    <div className='home'>
       <Navbar />
+      <Header />
     </div>
   )
 }
