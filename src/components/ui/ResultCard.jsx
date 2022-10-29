5dddb5a5bd323cbb7cad6bf979e726e2c9ad9330
Pratt <pratt.paredes@gmail.com>
2022-10-29 18:51:46 -0500

    adding functionality

--- src/components/ui/ResultCard.jsx
+++ src/components/ui/ResultCard.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import "./ResultCard.css";
 import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
 
-function ResultCard() {
+function ResultCard({ title, category, area, tags, image }) {
   return (
     <div className="resultCard">
       <div className="resultCard__container">
         <div className="resultCard__img--wrapper">
           <img
-            src="https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg"
+            src={image}
             alt=""
           />
           <div className="img__backdrop">
@@ -16,16 +16,16 @@ function ResultCard() {
           </div>
         </div>
         <div className="resultCard__description">
-          <h2>Spicy Arrabiata Penne</h2>
+          <h2>{title}</h2>
           <h3>
-            <span className="orange">Category: </span> Vegetarian
+            <span className="orange">Category: </span> {category}
           </h3>
           <h3>
-            <span className="orange">Area: </span> Italian
+            <span className="orange">Area: </span> {area}
           </h3>
-          <h4>
-            <span className="orange">Tags: </span> Pasta, Curry
-          </h4>
+          {tags && <h4>
+            <span className="orange">Tags: </span> {tags}
+          </h4>}
         </div>
       </div>
     </div>
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect } from 'react'
+import { useState } from 'react';
 import Navbar from '../components/Navbar'
 import SearchHeader from '../components/SearchHeader'
 import SearchResults from '../components/SearchResults'
 
 function Search() {
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  async function getRecipes() {
+    const { data } = await axios.get(
+      "https://www.themealdb.com/api/json/v1/1/search.php?s=chicken"
+    );
+
+    setRecipes(data.meals);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
   return (
     <div className='search'>
       <Navbar />
-      <SearchHeader />
-      <SearchResults />
+      <SearchHeader getRecipes={getRecipes}/>
+      <SearchResults recipes={recipes} loading={loading} getRecipes={getRecipes}/>
     </div>
   )
 }
